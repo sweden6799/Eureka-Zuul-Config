d9d36cf2795e0fb3021bed05e8456e3a3d4c4140
sunday679 <spring.example33@gmail.com>
2020-01-24 12:56:23 +0100

    eliminar

--- springboot-servicio-productos/src/main/java/com/atsistemas/examplespring/app/productos/models/dao/ProductoDao.java
+++
@@ -1,9 +0,0 @@
-package com.atsistemas.examplespring.app.productos.models.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.atsistemas.examplespring.app.productos.models.entity.Producto;
-
-public interface ProductoDao extends CrudRepository<Producto, Long>{
-
-}
